fcb1866d93e5a60be624d86be339dd420f8de0ce
Ralph Lorzano <ralorzano@my.cspc.edu.ph>
2024-08-13 15:20:14 +0800

    try 1

--- resources/views/faculty/instructor-dashboard.blade.php
+++ resources/views/faculty/instructor-dashboard.blade.php
@@ -177,41 +177,49 @@
                 fetch('/student-status-counts-chart')
                     .then(response => response.json())
                     .then(data => {
-                        var simplePieChartOptions = {
-                            chart: {
-                                width: 312,
-                                type: "pie",
-                                animations: {
-                                    enabled: true,
-                                    easing: 'easeinout',
-                                    speed: 800
+                        // Check if all series data is zero
+                        var isEmpty = data.regularCount === 0 && data.irregularCount === 0 && data.dropCount === 0;
+
+                        if (isEmpty) {
+                            // Display "No data available" if all counts are zero
+                            SimplePieChart.innerHTML = "<div style='text-align: center; padding-top: 100px;'>No data available</div>";
+                        } else {
+                            var simplePieChartOptions = {
+                                chart: {
+                                    width: 312,
+                                    type: "pie",
+                                    animations: {
+                                        enabled: true,
+                                        easing: 'easeinout',
+                                        speed: 800
+                                    },
                                 },
-                            },
-                            colors: ["#31ce3c", "#e9e300", "#cc0000"],
-                            labels: ["Regular", "Irregular", "Drop"],
-                            legend: {
-                                position: "top",
-                                horizontalAlign: "center",
-                                markers: {
-                                    radius: 0,
+                                colors: ["#31ce3c", "#e9e300", "#cc0000"],
+                                labels: ["Regular", "Irregular", "Drop"],
+                                legend: {
+                                    position: "top",
+                                    horizontalAlign: "center",
+                                    markers: {
+                                        radius: 0,
+                                    },
                                 },
-                            },
-                            series: [data.regularCount, data.irregularCount, data.dropCount],
-                            tooltip: {
-                                y: {
-                                    formatter: function(val) {
-                                        return val + " students";
+                                series: [data.regularCount, data.irregularCount, data.dropCount],
+                                tooltip: {
+                                    y: {
+                                        formatter: function(val) {
+                                            return val + " students";
+                                        }
                                     }
                                 }
-                            }
-                        };
+                            };
 
-                        var simplePieChartRenderer = new ApexCharts(
-                            SimplePieChart,
-                            simplePieChartOptions
-                        );
+                            var simplePieChartRenderer = new ApexCharts(
+                                SimplePieChart,
+                                simplePieChartOptions
+                            );
 
-                        simplePieChartRenderer.render();
+                            simplePieChartRenderer.render();
+                        }
                     });
             }
         });
